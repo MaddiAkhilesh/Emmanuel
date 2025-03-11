310190766fe0199ed242845720b1aeb09bd39289
maddiakhilesh7@gmail.com <maddiakhilesh7@gmail.com>
2025-03-11 13:55:37 +0530

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI3NTAxNw/spectral.js?branch_slug=main&token=d7905da4-19bd-47f6-9de5-ee336ea27839";
+export default { extends: ruleset };
